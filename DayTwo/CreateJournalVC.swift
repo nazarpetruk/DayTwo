1138130c85b17b5892f978b90bfdedf003c4aba0
nazar petruk <nazarpetruk15@gmail.com>
2019-10-08 18:18:25 +0200

    - starting storyboard config

---
+++ DayTwo/CreateJournalVC.swift
@@ -0,0 +1,30 @@
+//
+//  CreateJournalVC.swift
+//  DayTwo
+//
+//  Created by Nazar Petruk on 08/10/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class CreateJournalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
