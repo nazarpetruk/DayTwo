910d35d4a81c3f2359b19de2d4deb860dd923b65
nazar petruk <nazarpetruk15@gmail.com>
2019-10-08 18:58:15 +0200

    - adding header btns - config segues and btns

--- DayTwo/JournalTableVC.swift
+++ DayTwo/JournalTableVC.swift
@@ -9,26 +9,41 @@
 import UIKit
 
 class JournalTableVC: UITableViewController {
-
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var whiteCameraBtn: UIButton!
+    @IBOutlet weak var whitePlusBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        whitePlusBtn.imageView?.contentMode = .scaleAspectFit
+        whiteCameraBtn.imageView?.contentMode = .scaleAspectFit
     }
-
+    
+    //MARK: IBActions
+    @IBAction func cameraBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: "createNew", sender: "camera")
+    }
+    
+    
+    @IBAction func addBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: "createNew", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return 0
     }
 
