51fa3c7ff8bbd2bc5edd87d5aab84f88349d69b0
nazar petruk <nazarpetruk15@gmail.com>
2019-10-09 16:20:25 +0200

    - clipping bottom view to keyboard func

--- DayTwo/CreateJournalVC.swift
+++ DayTwo/CreateJournalVC.swift
@@ -20,6 +20,30 @@ class CreateJournalVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setDateBtn.layer.cornerRadius = 7
+        
+        //Notifications Observers
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    //MARK:ScrolltoKeyboardClipFunc
+    @objc func keyboardWillHide(notification:  Notification) {
+        changeKeyboardHightBack(notification: notification)
+    }
+    @objc func keyboardWillShow(notification:  Notification) {
+        changeKeyboardHight(notification: notification)
+    }
+    func changeKeyboardHight(notification: Notification){
+        if let keyBoardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyBoardHight = keyBoardFrame.cgRectValue.height
+            bottomConstraint.constant = keyBoardHight
+        }
+    }
+    func changeKeyboardHightBack(notification: Notification){
+        if let keyBoardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyBoardHight = keyBoardFrame.cgRectValue.height
+            bottomConstraint.constant = keyBoardHight - 55
+        }
     }
     
     //MARK: IBActions
